481432be4d8cecabe59156029ca70ede305b863f
sravan-7799 <sravankumarraikindi@gmail.com>
2025-09-03 01:00:25 +0530

    Update app.py

--- Smart Learning Analytics Platform/app.py
+++ Smart Learning Analytics Platform/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Student Performance Prediction App", page_icon="📊")
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
